34b30c0d205a596de5d3cc472a1d408f866a1e4f
Svampurinn <eina0307@stud.kea.dk>
2021-08-30 16:02:01 +0200

    !

---
+++ String-methods/3caPitalization.js
@@ -0,0 +1,6 @@
+const Name = "Melkorka";
+console.log(
+  Name.substring(0, 2).toLowerCase() +
+    Name.substring(2, 3).toUpperCase() +
+    Name.substring(3).toLowerCase()
+);
---
+++ String-methods/Capitalization.js
@@ -0,0 +1,2 @@
+const Name = "melkorka";
+console.log(Name.substring(0, 1).toUpperCase() + Name.substring(1));
